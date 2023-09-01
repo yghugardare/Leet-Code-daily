7118212ddb611c6d8085eefcf41c90a145271795
Yash Ghugardare <117991996+yghugardare@users.noreply.github.com>
2023-09-01 15:34:33 +0530

    Create CountBits.java

---
+++ CountBits.java
@@ -0,0 +1,17 @@
+class CountBits {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        int sub = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (sub * 2 == i) {
+                sub = i;
+            }
+
+            dp[i] = dp[i - sub] + 1;
+        }
+
+        return dp;        
+    }
+    
+}
